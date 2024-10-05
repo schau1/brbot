34f78603af3ad5371b3f7560b65f141fb80d53f4
schau1 <styrenev@gmail.com>
2024-10-04 17:34:17 -0700

    Update main.py
    Added new command and adjust database to save stages

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from curses.ascii import isdigit
+from re import I
+import re
 import discord
 import os
 import sys
@@ -9,6 +12,7 @@ from discord import app_commands
 from typing import Optional
 from discord.ext import tasks
 from dotenv import load_dotenv
+#from prettytable import PrettyTable
 
 # Loads the .env file that resides on the same level as the script.
 load_dotenv()
@@ -32,13 +36,8 @@ token = os.getenv('DISCORD_TOKEN')
 tree = app_commands.CommandTree(client)
 allowed_guilds = [TEST_DISCORD_GUILD, GUILD_ID_PRISON]
 allowed_channel = [TEST_CHANNEL_ID, GUILD_BATTLE_CHANNEL_ID]
-db = {"cantiga": 3, "akiria": 3, "codz": 3,
-      "nico": 3, "elementus": 3, "snoopy": 3, 
-      "knights of": 3, "flea": 3, "prinny": 3,
-      "chels": 3, "cyber": 3, "domova": 3,
-      "lexi": 3, "prinny": 3, "xal": 3,
-      "xalxal": 3, "zax": 3, "dk": 3,
-      "k1lo": 3, "stelio": 3, "ketch": 3}
+
+db = {}
 
 #reset at 2 a.m. 0 min 0 sec UTC - which is 7 p.m. PDT
 dt = datetime(2024, 8, 28, 2, 0, 0, tzinfo=timezone.utc)
@@ -46,6 +45,7 @@ dt = datetime(2024, 8, 28, 2, 0, 0, tzinfo=timezone.utc)
 #dt = datetime(2024, 8, 28, 1, 6, 0, tzinfo=timezone.utc)
 eventTime = dt.time()
 logFile = None
+assignmentLink = None
 
 # Make sure we have our Bot first
 if not token:
@@ -88,12 +88,83 @@ async def update_command(interaction, name: Optional[str] = None, attempts: Opti
     
     await interaction.response.send_message(update_message)
     
+# Command to update the remaining attempts of a user
+@tree.command(name="stage", description="Set score for stages")
+@app_commands.describe(name="The optional argument. If not provided, update member with your discord name")
+@app_commands.describe(stage1="The optional argument - number only. If not provided, change nothing")
+@app_commands.describe(stage2="The optional argument - number only. If not provided, change nothing")
+@app_commands.describe(stage3="The optional argument - number only. If not provided, change nothing")
+@app_commands.describe(stage4="The optional argument - number only. If not provided, change nothing")
+@app_commands.describe(stage5="The optional argument - number only. If not provided, change nothing")
+async def stage_command(interaction, name: Optional[str] = None, stage1: Optional[str] = None, stage2: Optional[str] = None, \
+                        stage3: Optional[str] = None, stage4: Optional[str] = None, stage5: Optional[str] = None):
+    if name is None:
+        name = interaction.user.global_name
+        
+    error = False
+
+    update_message = interaction.user.global_name + " updated scores for " + name + "."
+    
+    if stage1 is not None:
+        if re.match(r'^-?\d+(?:\.\d+)$', stage1) is None and not stage1.isdigit():
+            update_message += "\nIncorrect format. Please enter only number for stage1"
+            error = True
+    else:
+        stage1 = -1
+            
+    if stage2 is not None:
+        if re.match(r'^-?\d+(?:\.\d+)$', stage2) is None and not stage2.isdigit():
+            update_message += "\nIncorrect format. Please enter only number for stage2"
+            error = True            
+    else:
+        stage2 = -1            
+            
+    if stage3 is not None:
+        if re.match(r'^-?\d+(?:\.\d+)$', stage3) is None and not stage3.isdigit():
+            update_message += "\nIncorrect format. Please enter only number for stage3"
+            error = True            
+    else:
+        stage3 = -1            
+            
+    if stage4 is not None:
+        if re.match(r'^-?\d+(?:\.\d+)$', stage4) is None and not stage4.isdigit():
+            update_message += "\nIncorrect format. Please enter only number for stage4"
+            error = True            
+    else:
+        stage4 = -1            
+            
+    if stage5 is not None:
+        if re.match(r'^-?\d+(?:\.\d+)$', stage5) is None and not stage5.isdigit():
+            update_message += "\nIncorrect format. Please enter only number for stage5"
+            error = True            
+    else:
+        stage5 = -1            
+
+    if error == False:
+        if update_stages(name.lower(), float(stage1), float(stage2), float(stage3), float(stage4), float(stage5)) == False:
+            update_message = "\nERROR: '" + name + "' not found"
+        else:
+            update_message += composeScoreMessage(name.lower())
+            
+    global logFile        
+    logFile.write("\n" + update_message)
+    
+    await interaction.response.send_message(update_message)
+    
 # Command to show everyone from the database
-@tree.command(name="show", description="Show attempts left")#,             guild=discord.Object(id=GUILD_ID))
-async def show_command(interaction):
-    resp_message = composeRemainingMessage()
-    logFile.write("\n" + resp_message)    
-    await interaction.response.send_message(resp_message)
+@tree.command(name="show", description="Show the number of attempts left")#,             guild=discord.Object(id=GUILD_ID))
+@app_commands.describe(all="The optional argument. If not provided, display only members with attempts left.")
+async def show_command(interaction, all: Optional[bool] = False):
+    resp_message = composeRemainingMessage(all)
+    channel = client.get_channel(interaction.channel_id)   
+    
+    i = 0
+    for msg in resp_message:
+        if i == 0:
+            await interaction.response.send_message(msg)
+            i += 1
+        elif msg != "":
+            await channel.send(msg)   
 
 # Command to add a guild member to the database
 @tree.command(name="add", description="Add a guild member to the database")#,     guild=discord.Object(id=GUILD_ID))
@@ -101,7 +172,8 @@ async def add_command(interaction, name: str):
     resp_message = interaction.user.global_name + " added " + name + " to the battle ranking"
     if (add_member(name.lower()) == False):
         resp_message = name + " is already in the database"
-    logFile.write("\n" + resp_message)
+    if resp_message != None:        
+        logFile.write("\n" + resp_message)
     await interaction.response.send_message(resp_message)
 
 # Command to remove a guild member from the database
@@ -110,8 +182,48 @@ async def add_command(interaction, name: str):
 async def remove_command(interaction, name: str):
     resp_message = interaction.user.global_name + " removed " + name + " from the battle ranking"
     delete_member(name.lower())
-    logFile.write("\n" + resp_message)        
+    if resp_message != None:    
+        logFile.write("\n" + resp_message)        
     await interaction.response.send_message(resp_message)
+
+
+# Command to assign stage assignment and save to the database
+@tree.command(name="assign", description="Set stage assignment for the guild battle")#,
+#     guild=discord.Object(id=GUILD_ID))
+async def assign_command(interaction, link: str):
+    global assignmentLink
+    assignmentLink = link
+    resp_message = interaction.user.global_name + " set " + assignmentLink + " as stage assignment"
+    # write to the database - need to redesign the database a bit
+    if resp_message != None:    
+        logFile.write("\n" + resp_message)        
+    await interaction.response.send_message(resp_message)
+    
+# Command to show stage assignment
+@tree.command(name="showa", description="Show stage assignment for the guild battle")#,
+#     guild=discord.Object(id=GUILD_ID))
+async def showa_command(interaction):
+    resp_message = " Stage assignment: "
+# discord message format EX:  https://discord.com/channels/1273976720382234675/1288668100631330828/1289912810998071387
+#                             https://discordapp.com/channels/guild_id/channel_id/message_id
+    global assignmentLink
+    if (assignmentLink == None):
+        resp_message = "No stage assignment found."
+    else:
+        link = assignmentLink.split('/')
+        server_id = int(link[4])
+        channel_id = int(link[5])
+        msg_id = int(link[6])
+        server = client.get_guild(server_id)
+        channel = server.get_channel(channel_id)
+        message = await channel.fetch_message(msg_id)
+        resp_message = message.content
+        
+        #print(str(server_id) + " " + str(channel_id) + " " + str(msg_id))        
+        #print(message.content)
+      
+    await interaction.response.send_message(resp_message)
+
 
 # Task event that run at eventTime - Reset all attempts
 @tasks.loop(time=eventTime)
@@ -172,46 +284,131 @@ async def on_message(message):
 # Get current attempt for a member
 def getCurrentAttemps(name):
     if name in db.keys():
-        return db[name]
+        return db[name]["attempts"]
     else:
         return -1
 
 # Composes the message to be sent to the channel with all the remaining attempts
 # of the key-value pairs in the database
-def composeRemainingMessage():
-    resp_message = "Remaining attempts for today:\n"
+def composeRemainingMessage(allMembers):
+    final_message = []
+    resp_message = "Remaining attempts for today:"
     # Print all the keys from the database
-    keys = db.keys()   
+    keys = db.keys()    
+ 
+#    print(keys)
+#    print("\n")
+#    t = PrettyTable(['name', 'attempts', 'stage 1', 'stage 2', 'stage 3', 'stage 4', 'stage 5'])
+#    for key in sorted(keys):
+#        if db[key]["attempts"] > 0 or allMembers == True:
+#            t.add_row([key, db[key]["attempts"], db[key]["stage 1"], db[key]["stage 2"], db[key]["stage 3"], db[key]["stage 4"], db[key]["stage 5"]])
+#    resp_message = f"```{t}```"            
+#    print(t)
+           
+#    for key in sorted(keys):
+#        if db[key]["attempts"] > 0 or allMembers == True:
+#            resp_message += key + ": " + str(db[key]["attempts"]) + " [Stg 1: " + str(db[key]["stage 1"]) + "%, Stg 2:" + str(db[key]["stage 2"]) + \
+#                            "%, Stg 3: " + str(db[key]["stage 3"]) + "%, Stg 4: " + str(db[key]["stage 4"]) + "%, Stg 5: " + str(db[key]["stage 5"])
+#            resp_message += "%]\n"
     
+    count = 0
     for key in sorted(keys):
-        resp_message += key + ": " + str(db[key]) + "\n"
-    return resp_message
+        if db[key]["attempts"] > 0 or allMembers == True:
+#            resp_message += "\n" + key + ": " + str(db[key]["attempts"])
+#            resp_message += f"{"\n        Stg 1: " + str(db[key]["stage 1"]) + "%":>40}"
+#            resp_message += f"{"Stg 2: " + str(db[key]["stage 2"]) + "%":<40}"
+#            resp_message += f"{"\n        Stg 3: " + str(db[key]["stage 3"]) + "%":>40}"            
+#            resp_message += f"{"Stg 4: " + str(db[key]["stage 4"]) + "%":<40}"
+#            resp_message += "\n        Stg 5: " + str(db[key]["stage 5"]) + "%"
+            resp_message += "\n**" + key + "**: " + str(db[key]["attempts"])
+            resp_message += "\n     Stg 1: " + str(db[key]["stage 1"]) + "%"
+            resp_message += "\tStg 2: " + str(db[key]["stage 2"]) + "%"
+            resp_message += "\tStg 3: " + str(db[key]["stage 3"]) + "%"
+            resp_message += "\n     Stg 4: " + str(db[key]["stage 4"]) + "%"
+            resp_message += "\tStg 5: " + str(db[key]["stage 5"]) + "%"
+            count += 1
+            if count >= 15:   # more than 15 members
+                final_message.append(resp_message)
+                resp_message = ""
+                count = 0
+                
+    final_message.append(resp_message)        
+    return final_message
 
+def composeScoreMessage(name):
+    resp_message = "\nNew scores for " + name + ":"
+    if name in db.keys():
+        resp_message += "\n     Stg 1: " + str(db[name]["stage 1"]) + "%"
+        resp_message += "\tStg 2: " + str(db[name]["stage 2"]) + "%"
+        resp_message += "\tStg 3: " + str(db[name]["stage 3"]) + "%"
+        resp_message += "\n     Stg 4: " + str(db[name]["stage 4"]) + "%"
+        resp_message += "\tStg 5: " + str(db[name]["stage 5"]) + "%"
+    return resp_message
+    
+    
 # Reset all remaining attempts to MAX_ATTEMPTS
 def reset_attempts():
     keys = db.keys()
     for key in keys:
-        db[key] = MAX_ATTEMPTS
+        db[key]["attempts"] = MAX_ATTEMPTS
 
 # Update the remaining attempts of a member
 def update_attempts(name, attempts):
   if name in db.keys():
-    db[name] = attempts
+    db[name]["attempts"] = attempts
     return True
   else:
     return False
 
+# Update the stages of a member
+def update_stages(name, stage1, stage2, stage3, stage4, stage5):
+  if name in db.keys():
+    if stage1 >= 100:
+        db[name]["stage 1"] = 100
+    elif stage1 >= 0:
+        db[name]["stage 1"] = stage1
+
+    if stage2 >= 100:
+        db[name]["stage 2"] = 100        
+    elif stage2 >= 0:        
+        db[name]["stage 2"] = stage2
+        
+    if stage3 >= 100:
+        db[name]["stage 3"] = 100
+    elif stage3 >= 0:    
+        db[name]["stage 3"] = stage3
+        
+    if stage4 >= 100:
+        db[name]["stage 4"] = 100
+    elif stage4 >= 0:    
+        db[name]["stage 4"] = stage4
+        
+    if stage5 >= 100:
+        db[name]["stage 5"] = 100
+    elif stage5 >= 0:    
+        db[name]["stage 5"] = stage5
+    return True
+  else:
+    return False  
+
 # Add a member to the database
 def add_member(name):
     if name not in db:
-        db[name] = MAX_ATTEMPTS
+        db[name] = {}
+        db[name]["attempts"] = MAX_ATTEMPTS
+        db[name]["stage 1"] = 0
+        db[name]["stage 2"] = 0
+        db[name]["stage 3"] = 0
+        db[name]["stage 4"] = 0
+        db[name]["stage 5"] = 0        
         return True
     else:
         return False
 
 # Remove a member from the database
 def delete_member(name):
-    del db[name]
+    if name in db:
+        del db[name]
     if name not in db:
        logFile.write("\n" + name + " deleted successfully.")
 
@@ -219,7 +416,7 @@ def writeData(f):
     keys = db.keys()
     
     for key in keys:
-        data = key + "," + str(db[key]) + "\n"
+        data = key + "," + str(db[key]["attempts"]) + "," + str(db[key]["stage 1"]) + "," + str(db[key]["stage 2"]) + "," + str(db[key]["stage 3"]) + "," + str(db[key]["stage 4"]) + "," + str(db[key]["stage 5"]) + "\n"
         f.write(data)
 
 def readDataFromDatabase():
@@ -228,7 +425,17 @@ def readDataFromDatabase():
         for line in lines:            
             if line: # if line is not empty
                 data = line.split(',')
-                db[data[0]] = int(data[1])
+#                print(line)                    
+#                print(data)                
+#                print(str(data[1]))
+                db[data[0]]={}                
+                db[data[0]]["attempts"] = int(data[1])
+                db[data[0]]["stage 1"] = float(data[2])
+                db[data[0]]["stage 2"] = float(data[3])
+                db[data[0]]["stage 3"] = float(data[4])
+                db[data[0]]["stage 4"] = float(data[5])
+                db[data[0]]["stage 5"] = float(data[6])
+#        print(db)
     f.close()
     
 async def writeDataToDatabase():
